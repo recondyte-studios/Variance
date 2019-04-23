7ab1b6451d616ae6c72a12a6cf36d8ba9d1fcbba
varuss <oanhtran901@gmail.com>
2019-04-23 15:28:37 -0700

    work on enemies

--- script/SpiderWeb.gd
+++ script/SpiderWeb.gd
@@ -2,10 +2,9 @@ extends Area2D
 
 const SPEED = 300;
 var velocity = Vector2();
+onready var player = get_node("../player")
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
 func _physics_process(delta):
@@ -13,7 +12,15 @@ func _physics_process(delta):
 	translate(velocity);
 	
 
-
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+	pass # replace with function body
+
+
+func _on_SpiderWeb_body_entered(body):
+	if body.name == "player":
+		body.run_speed = body.run_speed - 50
+		if (body.run_speed == 100):
+			body.run_speed = 100;
+		queue_free();
 	pass # replace with function body
