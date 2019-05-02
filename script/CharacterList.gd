ee3e3007e7a13a636d136191c4e371da880bc71a
DKDonnor <phillipharden09@gmail.com>
2019-05-02 15:29:39 -0700

    Selcetion

---
+++ script/CharacterList.gd
@@ -0,0 +1,43 @@
+extends Node
+
+var VolcanStat = {
+	"Character" : "Volcan Forge",
+	"Str" : 35,
+	"Con" : 25,
+	"Dex" : 20,
+	"Teq" : 30,
+	"MaxHp" : 100,
+	"CurrentHp" : 100
+}
+
+var NoelStat = {
+	"Character" : "Noel Carbon",
+	"Str" : 40,
+	"Con" : 20,
+	"Dex" : 15,
+	"Teq" : 15,
+	"Pen" : 0.80,
+	"MaxHp" : 300,
+	"CurrentHp" : 300
+}
+
+var RimaStat = {
+	"Character" : "Rima Smokes",
+	"Str" : 10,
+	"Con" : 15,
+	"Dex" : 40,
+	"Teq" : 25,
+	"Pen" : 0.50,
+	"MaxHp" : 100,
+	"CurrentHp" : 100
+}
+
+var JacksonStat = {
+	"Character" : "Jackson Mori",
+	"Str" : 20,
+	"Con" : 30,
+	"Dex" : 15,
+	"Teq" : 15,
+	"MaxHp" : 200,
+	"CurrentHp" : 200
+}
--- script/SaveGame.gd
+++ script/SaveGame.gd
@@ -1,14 +1,14 @@
 extends Node
+#
+#func save():
+#	var save_dict = {
+#		"filename" : get_filename(),
+#		"parent": get_parent(),
+#		"Str" : strength,
+#		"Con" : constitution,
+#		"Dex" : dexterity,
+#		"Teq" : technique,
+#		"MaxHp" : MaxHp,
+#		"CurrentHp" : CurrentHP
+#	}
 
-func save():
-	var save_dict = {
-		"filename" : get_filename(),
-		"parent": get_parent(),
-		"Str" : strength,
-		"Con" : constitution,
-		"Dex" : dexterity,
-		"Teq" : technique,
-		"MaxHp" : MaxHp,
-		"CurrentHp" : CurrentHP
-	}
-	return save_dict;
