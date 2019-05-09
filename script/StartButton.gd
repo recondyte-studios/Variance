90b8d780329bffa2a1ceb460f3593ce3adfba1cb
DKDonnor <phillipharden09@gmail.com>
2019-05-09 15:19:12 -0700

    Fixed Button sizes. Just removed FileSelector from Button Parent and flipped Label to Parent of Button

--- script/StartButton.gd
+++ script/StartButton.gd
@@ -1,7 +1,7 @@
 extends VBoxContainer
 
 var save = preload("res://saves/GameSaver.gd")
-onready var FileId = get_node("GameFileSelect");
+onready var FileId = get_node("../GameFileSelect");
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://scene/CharacterSelect.tscn");
